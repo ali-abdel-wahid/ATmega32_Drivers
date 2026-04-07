6c78090dfb14531f8e680473fede38c33742bf56
ali_abdelwahed <alizain55347@gmail.com>
2026-04-07 11:34:07 +0200

    Add files via upload

---
+++ 02_HAL/04.SW/SW_config.h
@@ -0,0 +1,18 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<       SW_config.h        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* 
+*  Author: Ali Abdelwahed
+*   Layar : HAL
+*   SWC   : SW
+*
+*/
+
+#ifndef SW_CONFIG_H_
+#define SW_CONFIG_H_
+
+
+
+
+
+
+#endif /*SW_CONFIG_H_*/
---
+++ 02_HAL/04.SW/SW_private.h
@@ -0,0 +1,18 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<       SW_private.h        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* 
+*  Author: Ali Abdelwahed
+*   Layar : HAL
+*   SWC   : SW
+*
+*/
+
+#ifndef SW_PRIVATE_H_
+#define SW_PRIVATE_H_
+
+
+
+
+
+
+#endif /*SW_PRIVATE_H_*/
