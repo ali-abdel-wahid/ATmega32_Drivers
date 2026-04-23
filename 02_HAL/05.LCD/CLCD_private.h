32a904db915495178a94a501ee3020f1c1c3b191
ali_abdelwahed <alizain55347@gmail.com>
2026-04-23 15:46:11 +0200

    Add files via upload

---
+++ 02_HAL/05.LCD/CLCD_private.h
@@ -0,0 +1,20 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<       CLCD_private.h        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* 
+*  Author: Ali Abdelwahed
+*   Layar : HAL
+*   SWC   : CLCD
+*
+*/
+
+#ifndef CLCD_PRIVATE_H_
+#define CLCD_PRIVATE_H_
+
+static void CLCD_voidSendFallingEdge(void);
+
+
+
+
+#endif /*CLCD_PRIVATE_H_*/
+
+/*================================================ END ===================================================*/
