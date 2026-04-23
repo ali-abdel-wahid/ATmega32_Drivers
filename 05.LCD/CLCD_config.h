81eb4b49a39f76399d003a3d26b120e4cf9ae372
ali_abdelwahed <alizain55347@gmail.com>
2026-04-23 15:45:18 +0200

    Add files via upload

---
+++ 05.LCD/CLCD_config.h
@@ -0,0 +1,56 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<       CLCD_config.h        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* 
+*  Author: Ali Abdelwahed
+*   Layar : HAL
+*   SWC   : CLCD
+*
+*/
+
+#ifndef CLCD_CONFIG_H_
+#define CLCD_CONFIG_H_
+
+#include "DIO_config.h"
+
+
+/*
+Options:-
+   1- 4 BIT_MODE
+   2- 8 BIT_MODE
+   
+*/
+#define CLCD_MODE   8
+#define CLCD_MODE   4
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Options:-
+   1-DIO_PORTA
+   2-DIO_PORTB
+   3-DIO_PORTC
+   4-DIO_PORTD
+*/
+#define CLCD_DATA_PORT        DIO_PORTA
+/*RS,RW,EN*/
+#define CLCD_CONTROL_PORT     DIO_PORTC
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Options:-
+    1-DIO_PIN0
+    2-DIO_PIN1
+    3-DIO_PIN2
+    4-DIO_PIN3
+    5-DIO_PIN4
+    6-DIO_PIN5
+    7-DIO_PIN6
+    8-DIO_PIN7
+*/
+#define CLCD_RS    DIO_PIN0
+#define CLCD_RW    DIO_PIN1
+#define CLCD_EN    DIO_PIN2
+
+#endif /*CLCD_CONFIG_H_*/
+
+/*================================================ END ===================================================*/
